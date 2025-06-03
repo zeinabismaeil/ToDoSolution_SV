1ecd68ad575034e855e6308fdcd296e94358c476
Zeinab Ismaeil <zeinabismaeil296@example.com>
2025-06-03 14:47:37 +0300

    Added (Delete Task Feature)

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -3,43 +3,41 @@ using NTierTodoApp.Business;
 
 namespace NTierTodoApp.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly TaskService taskService;
+    public class HomeController : Controller
+    {
+        private readonly TaskService taskService;
 
-        public HomeController(TaskService service)
-        {
-            taskService = service;
-        }
+        public HomeController(TaskService service)
+        {
+            taskService = service;
+        }
 
-        public IActionResult Index()
-        {
-            var tasks = taskService.GetTasks();
-            return View(tasks);
-        }
+        public IActionResult Index()
+        {
+            var tasks = taskService.GetTasks();
+            return View(tasks);
+        }
 
-        [HttpPost]
-        public IActionResult AddTask(string title)
-        {
-            if (!string.IsNullOrWhiteSpace(title))
-                taskService.AddTask(title);
-            return RedirectToAction("Index");
-        }
+        [HttpPost]
+        public IActionResult AddTask(string title)
+        {
+            if (!string.IsNullOrWhiteSpace(title))
+                taskService.AddTask(title);
+            return RedirectToAction("Index");
+        }
 
-        [HttpPost]
-        public IActionResult CompleteTask(int id)
-        {
-            taskService.CompleteTask(id);
-            return RedirectToAction("Index");
-        }
+        [HttpPost]
+        public IActionResult CompleteTask(int id)
+        {
+            taskService.CompleteTask(id);
+            return RedirectToAction("Index");
+        }
 
-        // TODO: تنفيذ إجراء لحذف المهمة
-        [HttpPost]
-        public IActionResult DeleteTask(int id)
-        {
-            // TODO: استدعاء دالة حذف المهمة في TaskService
-
-            return RedirectToAction("Index");
-        }
-    }
+        [HttpPost]
+        public IActionResult DeleteTask(int id)
+        {
+            taskService.DeleteTask(id); // استدعاء دالة حذف المهمة في TaskService
+            return RedirectToAction("Index");
+        }
+    }
 }
